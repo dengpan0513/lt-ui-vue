49cb7212d3cc3d100f20b53954b2d849f9557cd6
dengdengdengpan <1306631506@qq.com>
2022-04-20 22:34:49 +0800

    add svg-sprite-loader

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require('path')
+
+function pathResolve (dir) {
+  return path.resolve(__dirname, dir)
+}
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  chainWebpack: config => {
+    config.module
+      .rule('svg')
+      .exclude
+      .add(pathResolve('icons'))
+      .end()
+  }
 })
