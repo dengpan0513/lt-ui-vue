586a62b8cad136ee1b69dcd446498a03acaf7cc1
dengdengdengpan <1306631506@qq.com>
2022-05-24 23:15:26 +0800

    完成 LHeader 和 LFooter

---
+++ components/Footer/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer class="l-layout-footer">
+    <slot />
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'LFooter'
+}
+</script>
+
+<style lang="scss" scoped>
+@use "../styles/variable.scss" as *;
+
+.l-layout-footer {
+  flex-shrink: 0;
+  padding: 16px 50px;
+  font-size: $font-size-small;
+}
+</style>
---
+++ components/Header/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header class="l-layout-header">
+    <slot />
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'LHeader'
+}
+</script>
+
+<style lang="scss" scoped>
+.l-layout-header {
+  flex-shrink: 0;
+  height: 64px;
+  padding-right: 50px;
+  padding-left: 50px;
+}
+</style>
