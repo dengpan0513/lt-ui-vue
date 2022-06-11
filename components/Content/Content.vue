1f353467c7262416a67fe282929c793b34ca8792
dengdengdengpan <1306631506@qq.com>
2022-06-11 10:53:28 +0800

    新增 LLayout 和 LContent 组件

---
+++ components/Content/Content.vue
@@ -0,0 +1,17 @@
+<template>
+  <main class="l-layout-content">
+    <slot />
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'LContent'
+}
+</script>
+
+<style lang="scss" scoped>
+.l-layout-content {
+  flex-grow: 1;
+}
+</style>
---
+++ components/Layout/Layout.vue
@@ -0,0 +1,43 @@
+<template>
+  <section :class="classObject" class="l-layout">
+    <slot />
+  </section>
+</template>
+
+<script>
+const classHasSider = 'l-layout-has-sider'
+
+export default {
+  name: 'LLayout',
+  data () {
+    return {
+      classObject: {
+        [classHasSider]: false
+      }
+    }
+  },
+  mounted () {
+    this.checkHasSider()
+  },
+  methods: {
+    checkHasSider () {
+      this.$children.forEach(instance => {
+        if (instance.$options.name === 'LSider') {
+          this.classObject[classHasSider] = true
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.l-layout {
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  &.l-layout-has-sider {
+    flex-direction: row;
+  }
+}
+</style>
